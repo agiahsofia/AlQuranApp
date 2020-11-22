78368107926f27334bf0e13a38e16c8bd112a4b6
NEC <sofiagiahig@gmail.com>
2020-11-22 15:01:49 +0700

    Initial commit

---
+++ app/src/main/java/com/agiah/alquranapp/DetailSurah.kt
@@ -0,0 +1,11 @@
+package com.agiah.alquranapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailSurah : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_surah)
+    }
+}
---
+++ app/src/main/java/com/agiah/alquranapp/ListSurahActivity.kt
@@ -0,0 +1,11 @@
+package com.agiah.alquranapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListSurahActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_surah)
+    }
+}
